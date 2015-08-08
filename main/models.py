26174b78511ecb9682d887df619ad0264548cab0
Kyan <ykyankai@gmail.com>
2015-08-08 17:22:08 +0800

    增加setting的main，增加models的user

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    question = models.TextField()
+    answer1 = models.CharField(max_length=128)
+    answer2 = models.CharField(max_length=128, blank=True, null=True)
+    tip = models.TextField(blank=True, null=True)
+    email = models.EmailField()
+    created = DateTimeField(default=timezone.now())
