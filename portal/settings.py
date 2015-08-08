5abe7af09e66646e3f851286f0024b780f24fedf
Kyan <ykyankai@gmail.com>
2015-08-08 19:25:05 +0800

    修改了uwsgi

--- portal/settings.py
+++ portal/settings.py
@@ -105,4 +105,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = BASE_DIR + '/static'
 STATIC_URL = '/static/'
+MEDIA_ROOT = BASE_DIR + '/media'
+MEDIA_URL = '/media/'
