6bed0a4d39c2cb64d04640a18c616e998c41f005
Kyan <ykyankai@gmail.com>
2015-08-10 18:52:23 +0800

    增加url

--- portal/urls.py
+++ portal/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from main import views
 
 urlpatterns = [
     # Examples:
@@ -7,4 +8,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home, name='home'),
 ]
