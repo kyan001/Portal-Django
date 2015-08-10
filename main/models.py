8fbd8fb29df866304e7f1f0ba43dd781eaa150cf
Kyan <ykyankai@gmail.com>
2015-08-10 18:25:50 +0800

    增加 nickname

--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=255, unique=True)
+    nickname = models.CharField(max_length=255, unique=True)
     question = models.TextField()
     answer1 = models.CharField(max_length=128)
     answer2 = models.CharField(max_length=128, blank=True, null=True)
