3545bdbe30741fec2df9a7bd68051803d6318592
Kyan <ykyankai@gmail.com>
2015-08-11 21:54:47 +0800

    重新实现

--- main/views.py
+++ main/views.py
@@ -31,16 +31,15 @@ def userUser(request):
     searchable_cols = ('username','id','email');
     try:
         for sc in searchable_cols:
-            colname = sc if sc in request.GET else None
-        if colname:
-            kwargs = {colname : request.GET.get(colname)}
-            user = User.objects.get(**kwargs);
-        else:
-            error_msg = "错误的参数：{0}\n".format(json.dumps(dict(request.GET)))
-            error_msg += "请输入 {0} 中的一种".format(', '.join(searchable_cols))
-            return infoMsg(error_msg, title='参数错误');
+            if sc in request.GET:
+                kwargs = {colname : request.GET.get(colname)}
+                user = User.objects.get(**kwargs);
+                context['headimg'] = user.getGravatar();
+                context['user'] = user
+                return render_to_response('user/index.html', context);
+        error_msg = "错误的参数：{0}\n".format(json.dumps(dict(request.GET)))
+        error_msg += "请输入 {0} 中的一种".format(', '.join(searchable_cols))
+        return infoMsg(error_msg, title='参数错误');
     except User.DoesNotExist:
-        return infoMsg("用户 {0} 不存在".format(str(request.GET)), title='参数错误')
-    context['headimg'] = user.getGravatar();
-    context['user'] = user
-    return render_to_response('user/index.html', context);
+        return infoMsg("用户 {0} 不存在".format(json.dumps(dict(request.GET)), title='参数错误')
+
