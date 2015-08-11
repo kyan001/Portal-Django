be598c20e891594a6c466399d4c5a6c050da882b
Kyan <ykyankai@gmail.com>
2015-08-11 15:25:54 +0800

    boostrap,imgs, views

--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import *
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello");
+def index(request):
+    return render_to_response("Hello");
+
+def userIndex(request, username):
+    context = {
+        'content' : "你的用户名是：" + username,
+        'title' : "用户名显示",
+    }
+    return render_to_response('msg.html', context);
--- portal/urls.py
+++ portal/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.index, name='index'),
+    url(r'^user/(?P<username>[a-zA-Z0-9_]+)/$', views.userIndex)
 ]
