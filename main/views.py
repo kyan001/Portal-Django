95e9d193e351013588ad95cfb6b8e702851e07a3
Kyan <ykyankai@gmail.com>
2015-08-11 21:11:49 +0800

    重新打印

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.template import *
 from main.models import *
+import json
 
 # Utils
 def infoMsg(content="Hi", url=None, title=None):
@@ -30,16 +31,16 @@ def userUser(request):
     searchable_cols = ('username','id','email');
     try:
         for sc in searchable_cols:
-            colname = sc if request.GET.get(sc) else None
+            colname = sc if request.has_key(sc) else None
         if colname:
             kwargs = {colname : request.GET.get(colname)}
             user = User.objects.get(**kwargs);
         else:
-            error_msg = "错误的参数：{0}\n".format(str(request.GET))
+            error_msg = "错误的参数：{0}\n".format(str(request.GET.items()))
             error_msg += "请输入 {0} 中的一种".format(', '.join(searchable_cols))
             return infoMsg(error_msg, title='参数错误');
     except User.DoesNotExist:
-        return infoMsg("用户 {0} 不存在").format(str(request.GET))
+        return infoMsg( "用户 {0} 不存在").format(json.dumps(request.GET)), title='参数错误')
     context['headimg'] = user.getGravatar();
     context['user'] = user
     return render_to_response('user/index.html', context);
