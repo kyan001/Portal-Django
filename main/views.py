cee0ce8b094c685bad97d2f31f34b2248b7c255e
Kyan <ykyankai@gmail.com>
2015-08-11 15:32:32 +0800

    init pics and update index.html

--- main/views.py
+++ main/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import *
 
+# Utils
+def infoMsg(content="Hi", url=None, title=None):
+    context = {
+        'title':title,
+        'content':content,
+        'url':url,
+    }
+    return render_template("msg.html", context);
+
 # Create your views here.
 def index(request):
     return render_to_response("Hello");
 
 def userIndex(request, username):
-    context = {
-        'content' : "你的用户名是：" + username,
-        'title' : "用户名显示",
-    }
-    return render_to_response('msg.html', context);
+    return infoMsg("您的用户名是：" + username)
