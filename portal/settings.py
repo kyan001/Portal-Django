a1ee4ec9b6b99d139afca567d89fbcbf12e50ca4
Kyan <ykyankai@gmail.com>
2015-08-15 16:08:20 +0800

    优化配置

--- portal/settings.py
+++ portal/settings.py
@@ -115,10 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_PATH = os.path.join(BASE_DIR, 'static') # 对应你的nginx配置
-STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    STATIC_PATH,
+    os.path.join(BASE_DIR, 'static')
 )
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
