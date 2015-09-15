fe6174fcfa0c8fb1628518af7fbcf9c6016ee80c
Kyan <ykyankai@gmail.com>
2015-09-15 18:24:59 +0800

    重新实现方法

--- static/js/KyanJsUtil.js
+++ static/js/KyanJsUtil.js
@@ -22,103 +22,87 @@ $.extend({
             },
             success: function(data){
                 var imgurl = "";
-                if(data.books[0]){
-                    if(data.books[0].title.indexOf(name) >= 0){
-                        switch(size){
-                            case 's':
-                                imgurl = data.books[0].images.small;
-                                break;
-                            case 'm':
-                                imgurl = data.books[0].images.medium;
-                                break;
-                            case 'l':
-                                imgurl = data.books[0].images.large;
-                                break;
-                            default:
-                                imgurl = data.books[0].images.medium;
-                        }
+                if( checkHas(name, data.books[0]) ){
+                    switch(size){
+                        case 's':
+                            imgurl = data.books[0].images.small;
+                            break;
+                        case 'm':
+                            imgurl = data.books[0].images.medium;
+                            break;
+                        case 'l':
+                            imgurl = data.books[0].images.large;
+                            break;
+                        default:
+                            imgurl = data.books[0].images.medium;
                     }
                 }
                 callback(imgurl);
             }
         });
     },
-    getBookPages: function(name, callback){
+    getBookInfo: function(name, callback){
         var book_search_api = 'https://api.douban.com/v2/book/search';
         if(name != ''){
             $.get(book_search_api, {'count':'1','q':name}, function(data){
-                var pages = ''
-                if(data.books[0]){
-                    if(data.books[0].title.indexOf(name) >= 0){
-                        pages = data.books[0].pages
-                    }
-                }
-                callback(pages);
-            }, 'jsonp');
-        }
-    },
-    getBookTags: function(name, callback){
-        var book_search_api = 'https://api.douban.com/v2/book/search';
-        if(name != ''){
-            $.get(book_search_api, {'count':'1','q':name}, function(data){
-                var tags = new Array()
-                if(data.books[0]){
-                    if(data.books[0].title.indexOf(name) >= 0){
-                        if(data.books[0].tags[0]){
-                            tags.push(data.books[0].tags[0].name)
-                        }
-                        if(data.books[0].tags[1]){
-                            tags.push(data.books[0].tags[1].name)
-                        }
-                        if(data.books[0].tags[2]){
-                            tags.push(data.books[0].tags[2].name)
-                        }
-                    }
-                }
-                callback(tags);
-            }, 'jsonp');
-        }
-    },
-    getBookRating: function(name, callback){
-        var book_search_api = 'https://api.douban.com/v2/book/search';
-        if(name != ''){
-            $.get(book_search_api, {'count':'1','q':name}, function(data){
+                var title = '';
                 var rating = '';
-                if(data.books[0]){
-                    if(data.books[0].title.indexOf(name) >= 0){
-                        rating = data.books[0].rating.average
-                    }
-                }
-                callback(rating)
-            }, 'jsonp');
-        }
-    },
-    getMovieTags: function(name, callback){
-        var movie_search_api = 'https://api.douban.com/v2/movie/search';
-        if(name != ''){
-            $.get(movie_search_api, {'count':'1','q':name}, function(data){
+                var pages = '';
                 var tags = new Array()
-                if(data.subjects[0]){
-                    if(data.subjects[0].title.indexOf(name) >= 0){
-                        tags = new Array(data.subjects[0].genres)
+                if( checkHas(name, data.books[0]) ){
+                    title = data.books[0].title
+                    if(data.books[0].tags[0]){
+                        tags.push(data.books[0].tags[0].name)
                     }
+                    if(data.books[0].tags[1]){
+                        tags.push(data.books[0].tags[1].name)
+                    }
+                    if(data.books[0].tags[2]){
+                        tags.push(data.books[0].tags[2].name)
+                    }
+                    rating = data.books[0].rating.average
+                    pages = data.books[0].pages
                 }
-                callback(tags);
+                callback(title, tags, rating, pages)
             }, 'jsonp');
         }
     },
-    getBookRating: function(name, callback){
+    getMovieInfo: function(name, callback){
         var movie_search_api = 'https://api.douban.com/v2/movie/search';
         if(name != ''){
             $.get(movie_search_api, {'count':'1','q':name}, function(data){
-                var rating = "";
-                if(data.subjects[0]){
-                    if(data.subjects[0].title.indexOf(name) >= 0){
-                        rating = data.subjects[0].rating.average
-                    }
+                var title = '';
+                var rating = '';
+                var tags = new Array()
+                if( checkHas(name, data.subjects[0]) ){
+                    title = data.subjects[0].title
+                    tags = data.subjects[0].genres
+                    rating = data.subjects[0].rating.average
                 }
-                callback(rating)
+                callback(title, tags, rating);
             }, 'jsonp');
+        }
+    },
+});
+
+function checkHas(keyword, bookOrMovie){
+    if(!bookOrMovie){
+        return false;
+    }
+    if(keyword){
+        keyword = keyword.toLowerCase()
+    } else {
+        return false;
+    }
+    if(bookOrMovie.title){
+        if(bookOrMovie.title.toLowerCase().indexOf(keyword) >= 0){
+            return true
         }
     }
-});
+    if(bookOrMovie.original_title){
+        if(bookOrMovie.original_title.toLowerCase().indexOf(keyword) >= 0){
+            return true
+        }
+    }
+    return false
+}
