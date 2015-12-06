2427578261c928aac3356a5c9b9e36020383c762
Kyan <ykyankai@gmail.com>
2015-12-06 12:04:41 +0800

    增加 readme 更新样式

--- static/js/progress/list_common.js
+++ static/js/progress/list_common.js
@@ -31,7 +31,7 @@ $(function(){
         var ele = $(this)
         $.getImageColor("", ele.attr('progressname'), function(result){
             if( result.color ){
-                ele.find('.panel').css('border', '2px solid' + result.color);
+                ele.find('.panel .panel-heading').css('border-left', '15px solid' + result.color);
             }
         });
     });
