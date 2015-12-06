5edace32c3d75a8d3c58576e9f76e424a89b585f
Kyan <ykyankai@gmail.com>
2015-12-06 23:42:54 +0800

    将颜色线挪至左边

--- static/js/progress/list_common.js
+++ static/js/progress/list_common.js
@@ -31,7 +31,7 @@ $(function(){
         var ele = $(this)
         $.getImageColor("", ele.attr('progressname'), function(result){
             if( result.color ){
-                ele.find('.panel').css('border-bottom', '5px solid' + result.color);
+                ele.find('.panel').css('border-left', '5px solid' + result.color);
             }
         });
     });
