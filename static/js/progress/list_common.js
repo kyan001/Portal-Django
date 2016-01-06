b4c8fd73a9dec1c787ae42d8cf3a8c05c6992f63
Kyan <ykyankai@gmail.com>
2016-01-06 23:42:40 +0800

    List 代码优化，结构优化。

--- static/js/progress/list_common.js
+++ static/js/progress/list_common.js
@@ -1,22 +1,18 @@
 $(function(){
     $('.progress-card .panel').mouseover(function(){
-        $(this).find('.progress').addClass('active').addClass('progress-striped');
+        $(this).find('.progress').addClass('active progress-striped');
     });
     $('.progress-card .panel').mouseout(function(){
-        $(this).find('.progress').removeClass('active').removeClass('progress-striped');
+        $(this).find('.progress').removeClass('active progress-striped');
     });
-    $('.progress-card .panel').click(function(){
-        var progressid = $(this).parent('.progress-card').attr('progressid');
-        window.location.href="/progress/detail?id=" + progressid;
-    });
-    $('.page-header').mouseover(function(){
-        $(this).find('h1>span').css('color','red');
-    });
-    $('.page-header').mouseout(function(){
-        $(this).find('h1>span').css('color','silver');
+    $('.progress-card .panel .weblink').click(function(){
+        var href = $(this).attr('href')
+        //window.location.href=href;
+        window.open(href)
+        return false
     });
     $('.page-header').click(function(){
-        var ele = $(this).find('h1>span');
+        var ele = $(this).find('span.glyphicon');
         var whenOpen = 'glyphicon-chevron-up';
         var whenClose = 'glyphicon-chevron-down';
         if(ele.hasClass(whenOpen)){
