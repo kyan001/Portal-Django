f8fb6afa16a8b651a0d2f88390f9a808bc7e2f25
Kyan <ykyankai@gmail.com>
2016-07-07 13:04:15 +0800

    更新了 uwsgiTool，start时不会因 & 符无法输入密码了

--- uwsgiTool.py
+++ uwsgiTool.py
@@ -1,58 +1,55 @@
 #!/usr/bin/python3
-#
-#=================================================================
-# uwsgiTool.py
-#
 # HISTORY
-#-----------------------------------------------------------------
+# -----------------------------------------------------------------
 #     DATE    |     AUTHOR     |  VERSION | COMMENT
-#-------------+----------------+----------+-----------------------
+# ------------+----------------+----------+-----------------------
 #  2015-01-13 |     YAN Kai    |   V1.0   | Script Creation
 #  2015-04-05 |     YAN Kai    |   V1.1   | detect running
 #  2015-08-07 |     YAN Kai    |   V1.2   | Merge
-#             |                |          |
-#-----------------------------------------------------------------
-#=================================================================
-#
+#  2016-07-07 |     YAN Kai    |   V1.3   | fix bug
+# -----------------------------------------------------------------
 import os
 import sys
 sys.path.append('/home/kyan001/KyanToolKit_Unix')
+
 import KyanToolKit_Py
 ktk = KyanToolKit_Py.KyanToolKit_Py()
 
-#--Pre-conditions Check-------------------------------------------
-ktk.needPlatform("linux");
+# -Pre-conditions Check-------------------------------------------
+ktk.needPlatform("linux")
 
-#--set params-----------------------------------------------------
-#config file
-uwsgi_xml="./uwsgi.xml"
+# -set params-----------------------------------------------------
+# config file
+uwsgi_xml = "./uwsgi.xml"
 if os.path.isfile(uwsgi_xml):
     ktk.info("uwsgi config file: " + uwsgi_xml)
 else:
     ktk.err("uwsgi config file not found: " + uwsgi_xml)
-#pid file
-pid_file="/var/run/uwsgi_portal_django.pid"
+# pid file
+pid_file = "/var/run/uwsgi_portal_django.pid"
 if os.path.exists(pid_file):
     ktk.warn("uwsgi is running @ " + pid_file)
 else:
     ktk.info("No uwsgi running")
-#choice
-operations=["start","stop","reload"];
-oprtn="";
+# choice
+operations = ["start", "stop", "reload"]
+oprtn = ""
 if len(sys.argv) != 2:
-    oprtn = ktk.getChoice(operations);
+    oprtn = ktk.getChoice(operations)
 elif sys.argv[1] in operations:
-    oprtn = sys.argv[1];
+    oprtn = sys.argv[1]
 else:
-    ktk.err("Wrong Params: " + sys.argv[1]);
-    ktk.byeBye();
-#--run commands---------------------------------------------------
+    ktk.err("Wrong Params: " + sys.argv[1])
+    ktk.byeBye()
+
+# -run commands---------------------------------------------------
 if "start" == oprtn:
-    ktk.runCmd("sudo uwsgi -x '" + uwsgi_xml +"' --pidfile '" + pid_file + "' &");
+    ktk.runCmd("sudo echo ''")
+    ktk.runCmd("sudo uwsgi -x '" + uwsgi_xml + "' --pidfile '" + pid_file + "' &")
 elif "stop" == oprtn:
-    ktk.runCmd("sudo uwsgi --stop " + pid_file);
+    ktk.runCmd("sudo uwsgi --stop " + pid_file)
 elif "reload" == oprtn:
-    ktk.runCmd("sudo uwsgi --reload " + pid_file);
+    ktk.runCmd("sudo uwsgi --reload " + pid_file)
 else:
-    ktk.err("Wrong operation: " + oprtn);
-    ktk.byeBye();
+    ktk.err("Wrong operation: " + oprtn)
+    ktk.byeBye()
