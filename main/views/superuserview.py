abb61b0509a4d1a2bcd07f22a7847a4b8de0f965
Kyan <ykyankai@gmail.com>
2016-09-24 01:23:33 +0800

    登录 -> 登入

--- main/views/superuserview.py
+++ main/views/superuserview.py
@@ -92,8 +92,8 @@ def superuserUpdatedb(request):
                 'category': 'signin',
                 'isallowed': False,
                 'image': '/static/media/badges/signin-no.png',
-                'description': '此用户被禁止登录',
-                'requirement': '当你做了什么事被关入小黑屋的时候，这枚徽章将会自动出现。\n（然而现在并没有什么小黑屋）\n一些不准许登录的系统用户也会拥有此徽章）',
+                'description': '此用户被禁止登入',
+                'requirement': '当你做了什么事被关入小黑屋的时候，这枚徽章将会自动出现。\n（然而现在并没有什么小黑屋）\n一些不准许登入的系统用户也会拥有此徽章）',
                 'designernname': '唯笑竹',
             },
             {
