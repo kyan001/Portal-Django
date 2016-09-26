24c7a00e949270123f0c9f91f50a141ac0737fac
Kyan <ykyankai@gmail.com>
2016-09-26 21:02:52 +0800

    使 user util 的返回值更有意义

--- util/user.py
+++ util/user.py
@@ -7,18 +7,18 @@ def getCurrentUser(request):
     """Get logged user from session"""
     userid = request.session.get(User.LOGIN_SESSION_KEY)
     if not userid:
-        return False
+        return None
     try:
         user = User.objects.get(id=userid)
     except User.DoesNotExist:
-        return None
+        return False
     return user
 
 
 def rememberLogin(request, user):
     """Remember logged user"""
     if not user:
-        return None
+        return False
     request.session[User.LOGIN_SESSION_KEY] = user.id  # remember login
     return True
 
