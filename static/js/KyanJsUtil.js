892975f887712b0e151b96fd6ec0eadd981f665d
Kyan <ykyankai@gmail.com>
2017-03-11 21:15:35 +0800

    不再 extend jQuery，而是增加了 OpusInfoGetter

--- static/js/KyanJsUtil.js
+++ static/js/KyanJsUtil.js
@@ -1,5 +1,11 @@
-$.extend({
-    getIsbnInfo: function(isbn, callback, callback404){
+var OpusInfoGetter = {
+    /**
+     * get book info from douban by isbn number
+     * @param  {string}     isbn     [isbn number]
+     * @param  {Function}   callback [call this function after got the data]
+     * @param  {Function}   callback [call this function if has a error ]
+     */
+    'getIsbnInfo': function(isbn, callback, callback404){
         var isbn_search_api = 'https://api.douban.com/v2/book/isbn/' + isbn
         if(isbn !== ''){
             $.ajax({
@@ -16,13 +22,21 @@ $.extend({
             })
         }
     },
-    getBookInfo: function(name, callback){
+    /**
+     * get book info from douban by the name
+     * @param  {string}     name     [book's name, full or partial]
+     * @param  {Function}   callback [call this function after got the data]
+     */
+    'getBookInfo': function(name, callback){
         var book_search_api = '/opus/searchopusinfo'
         var book_search_api_douban = 'https://api.douban.com/v2/book/search';
         if(name !== ''){
             $.get(book_search_api, {'count':'1','q':name, 'type':'book'}, function(data){
                 var info = {}
-                if(data.books[0]){
+                info.exist = data.books[0] ? true : false
+                info.type = 'book'
+                info.api = book_search_api_douban + '?count=1&q=' + name
+                if(info.exist){
                     info.title = data.books[0].title;
                     info.origin_title = data.books[0].origin_title  // book only, optional
                     info.numrater = data.books[0].rating.numRaters  // book only
@@ -47,15 +61,17 @@ $.extend({
                     }
                     info.image = data.books[0].image
                 }
-                info.api = book_search_api_douban + '?count=1&q=' + name
-                info.type = 'book'
-                info.match = checkHas(name, data.books[0])
-                info.exist = data.books[0]?true:false
+                info.match = OpusInfoGetter.checkHas(name, data.books[0])
                 callback(info)
             }, 'json');
         }
     },
-    getMovieInfo: function(name, callback){
+    /**
+     * get movie info from douban by the name
+     * @param  {string}     name     [movie's name, full or partial]
+     * @param  {Function}   callback [call this function after got the data]
+     */
+    'getMovieInfo': function(name, callback){
         var movie_search_api = '/opus/searchopusinfo'
         var movie_search_api_douban = 'https://api.douban.com/v2/movie/search';
         var movie_info_api = 'https://api.douban.com/v2/movie/subject/';
@@ -80,7 +96,7 @@ $.extend({
                 }
                 info.api = movie_search_api_douban + '?count=1&q=' + name
                 info.type = 'movie'
-                info.match = checkHas(name, data.subjects[0])
+                info.match = OpusInfoGetter.checkHas(name, data.subjects[0])
                 info.exist = data.subjects[0]?true:false
                 callback(info);
             }, 'json');
@@ -88,11 +104,11 @@ $.extend({
     },
     /**
      * get image's primary color
-     * @param  {[string]}   url      [image url, if url=="", only read cache]
-     * @param  {[string]}   name     [the opus's title which contains the image]
+     * @param  {string}     url      [image url, if url=="", only read cache]
+     * @param  {string}     name     [the opus's title which contains the image]
      * @param  {Function}   callback [call this function after got the data]
      */
-    getImageColor: function(url, opusid, callback){
+    'getImageColor': function(url, opusid, callback){
         var request_data = {}
         if(url==""){
             request_data = {
@@ -115,39 +131,41 @@ $.extend({
             }
         });
     },
-});
-
-function checkHas(keywords, bookOrMovie){
-    if(!bookOrMovie){
-        return false;
-    }
-    if(!keywords){
-        return false;
-    }
-    kw = keywords.toLowerCase().split(' ')
-    var isInOriginalTitle = true;
-    var isInTitle = true;
-    if(bookOrMovie.original_title){//有英文名
-        for(var i in kw){
-            if(bookOrMovie.original_title.toLowerCase().indexOf(kw[i]) < 0){
-                isInOriginalTitle = false //只要有一个词不在title内，则判定失败
-                break
-            }
+    /**
+     * check if the keyword is appeared in the original name or title of a  book/movie
+     * @param  {string}     keyword     [words, mostly is short]
+     * @param  {opusinfo}   bookOrMovie [the opus's info, should include a title or original_title]
+     * @return {bool}                   [if the keyword is in the opus names]
+     */
+    'checkHas': function(keyword, bookOrMovie){
+        if(!keyword || !bookOrMovie){
+            return false;
         }
-    } else {//无英文名
-        isInOriginalTitle = false;
-    }
-    if(bookOrMovie.title){ //如果有title
-        for(var i in kw){
-            if(bookOrMovie.title.toLowerCase().indexOf(kw[i]) < 0){
-                isInTitle = false //只要有一个词不在title内，则判定失败
-                break
+        var kw = keyword.toLowerCase().split(' ')
+        var isInOriginalTitle = true;
+        var isInTitle = true;
+        if(bookOrMovie.original_title){//有英文名
+            for(var i in kw){
+                if(bookOrMovie.original_title.toLowerCase().indexOf(kw[i]) < 0){
+                    isInOriginalTitle = false //只要有一个词不在title内，则判定失败
+                    break
+                }
             }
+        } else {//无英文名
+            isInOriginalTitle = false;
         }
-    } else { //没有title
-        isInTitle = false
-    }
-    return isInOriginalTitle || isInTitle
+        if(bookOrMovie.title){ //如果有title
+            for(var i in kw){
+                if(bookOrMovie.title.toLowerCase().indexOf(kw[i]) < 0){
+                    isInTitle = false //只要有一个词不在title内，则判定失败
+                    break
+                }
+            }
+        } else { //没有title
+            isInTitle = false
+        }
+        return isInOriginalTitle || isInTitle
+    },
 }
 
 function disableAllBtn(){
