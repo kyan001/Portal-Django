b6a424b2e10bedd68f886e94b191ad2ad7872215
Kyan <ykyankai@gmail.com>
2017-09-11 21:00:53 +0800

    JS format refined

--- static/js/KyanJsUtil.js
+++ static/js/KyanJsUtil.js
@@ -13,7 +13,7 @@ var OpusInfoGetter = {
                 url: isbn_search_api,
                 data: {},
                 dataType: 'jsonp',
-                error: function() {
+                error: function(){
                     callback404()
                 },
                 success: function(result){
@@ -29,7 +29,7 @@ var OpusInfoGetter = {
      */
     'getBookInfo': function(name, callback){
         var book_search_api = '/opus/searchopusinfo'
-        var book_search_api_douban = 'https://api.douban.com/v2/book/search';
+        var book_search_api_douban = 'https://api.douban.com/v2/book/search'
         if(name !== ''){
             $.get(book_search_api, {'count':'1','q':name, 'type':'book'}, function(data){
                 var info = {}
@@ -73,8 +73,8 @@ var OpusInfoGetter = {
      */
     'getMovieInfo': function(name, callback){
         var movie_search_api = '/opus/searchopusinfo'
-        var movie_search_api_douban = 'https://api.douban.com/v2/movie/search';
-        var movie_info_api = 'https://api.douban.com/v2/movie/subject/';
+        var movie_search_api_douban = 'https://api.douban.com/v2/movie/search'
+        var movie_info_api = 'https://api.douban.com/v2/movie/subject/'
         if(name != ''){
             $.get(movie_search_api, {'count':'1','q':name, 'type':'movie'}, function(data){
                 var info = {}
@@ -98,7 +98,7 @@ var OpusInfoGetter = {
                 info.type = 'movie'
                 info.match = OpusInfoGetter.checkHas(name, data.subjects[0])
                 info.exist = data.subjects[0]?true:false
-                callback(info);
+                callback(info)
             }, 'json');
         }
     },
@@ -144,24 +144,24 @@ var OpusInfoGetter = {
         var kw = keyword.toLowerCase().split(' ')
         var isInOriginalTitle = true;
         var isInTitle = true;
-        if(bookOrMovie.original_title){//有英文名
+        if(bookOrMovie.original_title){  // 有英文名
             for(var i in kw){
                 if(bookOrMovie.original_title.toLowerCase().indexOf(kw[i]) < 0){
-                    isInOriginalTitle = false //只要有一个词不在title内，则判定失败
+                    isInOriginalTitle = false  // 只要有一个词不在title内，则判定失败
                     break
                 }
             }
-        } else {//无英文名
-            isInOriginalTitle = false;
+        } else {  // 无英文名
+            isInOriginalTitle = false
         }
-        if(bookOrMovie.title){ //如果有title
+        if(bookOrMovie.title){  // 如果有title
             for(var i in kw){
                 if(bookOrMovie.title.toLowerCase().indexOf(kw[i]) < 0){
-                    isInTitle = false //只要有一个词不在title内，则判定失败
+                    isInTitle = false  // 只要有一个词不在title内，则判定失败
                     break
                 }
             }
-        } else { //没有title
+        } else {  // 没有title
             isInTitle = false
         }
         return isInOriginalTitle || isInTitle
