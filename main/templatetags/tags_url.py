dd4ff2291c1c8b0680ba60d4357bc820baa76a29
Kyan <ykyankai@gmail.com>
2017-11-28 00:41:01 +0800

    add more protocal detect to "isurl" filter

--- main/templatetags/tags_url.py
+++ main/templatetags/tags_url.py
@@ -1,10 +1,11 @@
 from django import template
 register = template.Library()
 
+
 @register.filter
 def isurl(value):
     input_string = str(value)
-    protocals = ['http','ftp','https']
+    protocals = ['http', 'ftp', 'https', 'file', 'magnet', 'thunder', 'ed2k']
     for p in protocals:
         if input_string.startswith(p + '://'):
             return True
