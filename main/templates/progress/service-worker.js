a9b836cf7dd67632bdcb191d369eeeea681af20a
Kyan <ykyankai@gmail.com>
2019-04-09 17:33:41 +0800

    add progress-search/archive to sw.js

--- main/templates/progress/service-worker.js
+++ main/templates/progress/service-worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'progress-cache-v1.0.1'
+var CACHE_NAME = 'progress-cache-v1.1.2'
 var urlsCacheFirst = [
     "/static/3rd/jquery/jquery-3.3.1.min.js",
     "/static/3rd/bootstrap-3.4.1/js/bootstrap.min.js",
@@ -16,6 +16,9 @@ var urlsCacheFirst = [
 var urlsOnlineFirst = [
     "/progress/list",
     "/progress/list/",
+    "/progress/archive",
+    "/progress/archive/",
+    "/progress/search",
     "/user/getunreadcount",
     "/user/getunreadcount/",
 ]
