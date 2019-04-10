c5896a73bde00969eb006d4bb843cb25deb5e439
Kyan <ykyankai@gmail.com>
2019-04-10 11:00:01 +0800

    move progress/common.js/.css to progress/progresslist.js/.css

--- main/templates/progress/service-worker.js
+++ main/templates/progress/service-worker.js
@@ -1,13 +1,13 @@
-var CACHE_NAME = 'progress-cache-v1.1.5'
+var CACHE_NAME = 'progress-cache-v1.1.6'
 var urlsCacheFirst = [
     "/static/3rd/jquery/jquery-3.3.1.min.js",
     "/static/3rd/bootstrap-3.4.1/js/bootstrap.min.js",
     "/static/js/KyanJsUtil.js?version=1.5.1",
     "/static/3rd/bootstrap-3.4.1/css/bootstrap.min.css",
     "/static/css/KyanCssUtil.css?version=1.5.1",
-    "/static/css/progress/list/common.css?version=2.2.2",
+    "/static/css/progress/progresslist.css?version=2.2.2",
     "/static/css/progress/progresscard.css?version=2.3.1",
-    "/static/js/progress/list/common.js?version=2.2.2",
+    "/static/js/progress/progresslist.js?version=2.2.2",
     "/static/3rd/instant.page/instantpage-1.2.2.js",
     "/static/3rd/bootstrap-3.4.1/fonts/glyphicons-halflings-regular.woff2",
     "/static/img/Logo_List.png",
