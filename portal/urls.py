0a1b356a95146868bb6b70f51999ff82b7d808e0
Kyan <ykyankai@gmail.com>
2024-03-12 19:09:11 +0800

    urls now using path instead of url

--- portal/urls.py
+++ portal/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import include, url
-from django.urls import path
+from django.urls import include, path
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.conf import settings
@@ -17,34 +16,34 @@ import main.views.tool
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'portal.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # path('', 'portal.views.home', name='home'),
+    # path('blog/', include('blog.urls')),
 
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     # I18N
     path('i18n/', include('django.conf.urls.i18n')),
     # INDEX
-    url(r'^$', main.views.index.index, name='index'),
-    url(r'^index/settheme$', main.views.index.settheme),
-    url(r'^index/help$', main.views.index.help),
+    path('', main.views.index.index, name='index'),
+    path('index/settheme', main.views.index.settheme),
+    path('index/help', main.views.index.help),
     # USER
-    url(r'^user/public$', main.views.user.public),
-    url(r'^user/profile', main.views.user.profile),
-    url(r'^user/exphistory$', main.views.user.exphistory),
-    url(r'^user/signin$', main.views.user.signin),  # GET
-    url(r'^user/signup$', main.views.user.signup),
-    url(r'^user/headimgupdate$', main.views.user.headimgUpdate),  # POST
-    url(r'^user/forgetanswer$', main.views.user.forgetAnswer),
-    url(r'^user/forgetusername$', main.views.user.forgetUsername),  # POST
-    url(r'^user/checklogin$', main.views.user.checkLogin),  # POST
-    url(r'^user/getunreadcount$', main.views.user.getUnreadCount),
-    url(r'^user/newuser$', main.views.user.newUser),  # POST
-    url(r'^user/logout$', main.views.user.logout),
-    url(r'^user/validateusername$', main.views.user.validateUsername),
-    url(r'^user/validatenickname$', main.views.user.validateNickname),
-    url(r'^user/validateemail$', main.views.user.validateEmail),
-    url(r'^user/getquestionandtip$', main.views.user.getQuestionAndTip),
-    url(r'^user/setting$', main.views.user.setting),
+    path('user/public', main.views.user.public),
+    path('user/profile', main.views.user.profile),
+    path('user/exphistory', main.views.user.exphistory),
+    path('user/signin', main.views.user.signin),  # GET
+    path('user/signup', main.views.user.signup),
+    path('user/headimgupdate', main.views.user.headimgUpdate),  # POST
+    path('user/forgetanswer', main.views.user.forgetAnswer),
+    path('user/forgetusername', main.views.user.forgetUsername),  # POST
+    path('user/checklogin', main.views.user.checkLogin),  # POST
+    path('user/getunreadcount', main.views.user.getUnreadCount),
+    path('user/newuser', main.views.user.newUser),  # POST
+    path('user/logout', main.views.user.logout),
+    path('user/validateusername', main.views.user.validateUsername),
+    path('user/validatenickname', main.views.user.validateNickname),
+    path('user/validateemail', main.views.user.validateEmail),
+    path('user/getquestionandtip', main.views.user.getQuestionAndTip),
+    path('user/setting', main.views.user.setting),
     # PROGRESS
     path('progress/service-worker.js', TemplateView.as_view(template_name='progress/service-worker.js', content_type='application/javascript')),  # PWA sw.js
     path('progress/manifest.json', TemplateView.as_view(template_name='progress/manifest.json', content_type='application/json')),  # PWA manifest.json
@@ -65,27 +64,27 @@ urlpatterns = [
     path('progress/setsettings', main.views.progress.setsettings),  # POST
     path('progress/export', main.views.progress.export),  # GET
     # OPUS
-    url(r'^opus/detail$', main.views.opus.detail),  # GET
-    url(r'^opus/searchopusinfo$', main.views.opus.searchOpusInfo),  # GET Ajax
-    url(r'^opus/getopuswordcloud$', main.views.opus.getOpusWordCloud),  # GET Ajax
-    url(r'^opus/importfrom$', main.views.opus.importFrom),  # GET
+    path('opus/detail', main.views.opus.detail),  # GET
+    path('opus/searchopusinfo', main.views.opus.searchOpusInfo),  # GET Ajax
+    path('opus/getopuswordcloud', main.views.opus.getOpusWordCloud),  # GET Ajax
+    path('opus/importfrom', main.views.opus.importFrom),  # GET
     # CHAT
-    url(r'^chat/inbox$', main.views.chat.inbox),
-    url(r'^chat/markread$', main.views.chat.markread),
-    url(r'^chat/delete$', main.views.chat.delete),
-    url(r'^chat/send$', main.views.chat.send),
-    url(r'^chat/conversation$', main.views.chat.conversation),
+    path('chat/inbox', main.views.chat.inbox),
+    path('chat/markread', main.views.chat.markread),
+    path('chat/delete', main.views.chat.delete),
+    path('chat/send', main.views.chat.send),
+    path('chat/conversation', main.views.chat.conversation),
     # badge
-    url(r'^badge/list$', main.views.badge.list),
-    url(r'^badge/detail$', main.views.badge.detail),  # GET
+    path('badge/list', main.views.badge.list),
+    path('badge/detail', main.views.badge.detail),  # GET
     # robo talk
-    url(r'^robotalk$', main.views.robotalk.index),
-    url(r'^robotalk/getresponse$', main.views.robotalk.getResponse),  # GET
+    path('robotalk', main.views.robotalk.index),
+    path('robotalk/getresponse', main.views.robotalk.getResponse),  # GET
     # small tools
     path('tool/idvarify', main.views.tool.idVarify),  # GET Ajax
     # SUPERUSER
-    url(r'^superuser/index$', main.views.superuser.index),
-    url(r'^superuser/broadcast$', main.views.superuser.broadcast),
-    url(r'^superuser/sendbroadcast$', main.views.superuser.sendbroadcast),
-    url(r'^superuser/updatedb$', main.views.superuser.updatedb),
+    path('superuser/index', main.views.superuser.index),
+    path('superuser/broadcast', main.views.superuser.broadcast),
+    path('superuser/sendbroadcast', main.views.superuser.sendbroadcast),
+    path('superuser/updatedb', main.views.superuser.updatedb),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # add media folder as static files
